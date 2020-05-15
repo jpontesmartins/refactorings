426de17a628acdf63c5e1c55352822ce72305cc1
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-15 12:10:45 -0300

    defaut sqfm

--- sqfm/sqfm.js
+++ sqfm/sqfm.js
@@ -3,9 +3,7 @@
 // Separate Query from Modifier
 
 // Objetivo: remover efeitos colaterais deixando uma função pura
-
-// Dica: qualquer função quer retorne algum valor 
-// não deve ter efeitos colaterais.
+// Dica: qualquer função quer retorne algum valor não deve ter efeitos colaterais.
 
 function alertForMiscreant(people) {
     for (const p of people) {
