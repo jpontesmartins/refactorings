8e40e6d37fa3f1accd7ea77ca2aa48fd336f1078
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-15 14:33:39 -0300

    inline function default

--- inline-function/inline-function.js
+++ inline-function/inline-function.js
@@ -7,14 +7,22 @@
 
 function reportLines(aCustomer) {
     const lines = [];
-    lines.push(["name", aCustomer.name]);
-    lines.push(["location", aCustomer.location]);
+    gatherCustomerData(lines, aCustomer);
     return lines;
 }
 
-module.exports = {
-    reportLines
+function gatherCustomerData(out, aCustomer) {
+    out.push(["name", aCustomer.name]);
+    out.push(["location", aCustomer.location]);
 }
+
+const aCustomer = {
+    name: "John",
+    location: "City"
+}
+
+console.log(reportLines(aCustomer));
+
 
 
 
