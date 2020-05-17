f5dbf305b19a5f90c1da7b32370f6a256442c998
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-17 14:49:57 -0300

    replace temp with query

---
+++ replace-temp-with-query/order-price.js
@@ -0,0 +1,19 @@
+
+
+class Order {
+    constructor(quantity, item) {
+        this._quantity = quantity;
+        this._item = item;
+    }
+
+    getPrice() {
+        const basePrice = this._quantity * this._item;
+        const discountFactor = 0.98;
+        if (basePrice > 1000) {
+            discountFactor -= 0.03;
+            return basePrice * discountFactor;
+        }
+
+    }
+
+}
