be81f4be7578ec21432c3ae4d85f866ffcbd5194
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-20 09:11:39 -0300

    charge calculator refactored with replace-command-with-function

--- replace-command-with-function/refactoring/charge-calulator.js
+++ replace-command-with-function/refactoring/charge-calulator.js
@@ -4,28 +4,12 @@
 
 // Objetivo: simplificar
 
-class ChargeCalculator {
-    constructor(customer, usage, provider) {
-        this._customer = customer;
-        this._usage = usage;
-        this._provider = provider;
-    }
-
-    getBaseCharge() {
-        return this._customer.baseRate * this._usage;
-    }
-
-    getCharge() {
-        const baseCharge = this._customer.baseRate * this._usage; 
-        return baseCharge + this._provider.connectionCharge;
-    }
-}
-
 function charge(customer, usage, provider) {
-    return new ChargeCalculator(customer, usage, provider).getCharge();
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
 }
 
 module.exports = {
     charge
-};
+}
 
