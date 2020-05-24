0c56ae5f236d2eb3fc898f807c708f22561e0e8f
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-24 09:11:47 -0300

    more using local variable

--- extract-function/refactoring/extract-function.js
+++ extract-function/refactoring/extract-function.js
@@ -17,13 +17,16 @@ function printOwing(invoice) {
     }
 
     //record due date
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
+    recordDueDate(invoice);
 
     //print details
     printDetails(invoice, outstanding);
 
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 }
 
 function printDetails(invoice, outstanding) {
