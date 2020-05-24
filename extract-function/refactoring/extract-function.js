0265e5a5b16bef082052f146d944311d0060adbc
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-24 09:08:42 -0300

    no variables out of scope

---
+++ extract-function/refactoring/extract-function.js
@@ -0,0 +1,35 @@
+// Refatoração
+
+// Extract Function
+
+// Objetivo: 
+
+// No variables out of scope
+
+function printOwing(invoice) {
+    let outstanding = 0;
+
+    printBanner();
+
+    //calculate outstanding
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+
+    //record due date
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+
+    //print details
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+
+}
+
+function printBanner() {
+    console.log('-------------');
+    console.log('customer owes');
+    console.log('-------------');
+}
