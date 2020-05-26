5769bdd80d7552e401a61b31d879f84707511d23
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-26 11:38:31 -0300

    with console.log

--- move-statements-into-functions/render-html.js
+++ move-statements-into-functions/render-html.js
@@ -2,23 +2,25 @@
 
 // Move Statements into Function
 
-// Objetivo: Remover duplicação. As linhas 12-13 e 27-28 são semanticamente equivalentes.
+// Objetivo: Remover duplicação. As linhas 14-15 e 29-30 são semanticamente equivalentes.
 
 
 function renderPerson(outStream, person) {
     const result = [];
     result.push(`<p>${person.name}</p>`);
     result.push(renderPhoto(person.photo));
-    result.push(`<p> title: ${person.photo.title}`); //titulo..
+    result.push("...other info..");
+    result.push("...other info..");
+    result.push(`<p> title: ${person.photo.title}  </p>`); //titulo..
     result.push(emitPhotoData(person.photo)); //dados da foto
     return result.join("\n");
 }
 
 function renderPhoto(photo) {
-    // ...
-    photoDiv(photo);
-    // ...
-    return photo;
+    return [
+        "<img src...> ",
+        photoDiv(photo)
+    ].join("\n");
 }
 
 function photoDiv(photo) {
@@ -32,7 +34,24 @@ function photoDiv(photo) {
 
 function emitPhotoData(aPhoto) {
     const result = [];
-    result.push(`<p> location: ${aPhoto.location}`);
-    result.push(`<p> date: ${aPhoto.date.toDateString()}`);
+    result.push(`<p> location: ${aPhoto.location} </p>`);
+    result.push(`<p> date: ${aPhoto.date.toDateString()} </p>`);
     return result.join("\n");
 }
+
+
+// para reproduzir
+
+let outStream = "";
+
+const person = {
+    name: "John",
+    photo: {
+        title: "John's Picture",
+        location: "park",
+        date: new Date()
+    }
+};
+
+console.log(renderPerson(outStream, person));
+
