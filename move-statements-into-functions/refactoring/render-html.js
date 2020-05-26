350d3f073764a98997f32bada6fe518e34b9eb24
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-26 16:46:30 -0300

    inline-function to emitPhotoData inside new xxnew

--- move-statements-into-functions/refactoring/render-html.js
+++ move-statements-into-functions/refactoring/render-html.js
@@ -34,15 +34,12 @@ function photoDiv(photo) {
 function xxnew(photo) {
     return [
         `<p> title: ${photo.title} </p>`, //titulo..
-        emitPhotoData(photo), //dados da foto
+        `<p> location: ${photo.location} </p>`,
+        `<p> date: ${photo.date.toDateString()} </p>`
     ].join("\n");
 }
 
 function emitPhotoData(aPhoto) {
-    const result = [];
-    result.push(`<p> location: ${aPhoto.location} </p>`);
-    result.push(`<p> date: ${aPhoto.date.toDateString()} </p>`);
-    return result.join("\n");
 }
 
 module.exports = {
