e95b6ea75baf5de21b5bbf68599723dde92b2bca
ovelha estoica <ovelhaestoica@bitbucket.org>
2020-05-26 17:18:59 -0300

    refactored but has no tests

--- move-statements-into-callers/refactoring/render-html.js
+++ move-statements-into-callers/refactoring/render-html.js
@@ -8,7 +8,8 @@
 function renderPerson(outStream, person){
     outStream.write(`<p>${person.name}</p>`);
     renderPhoto(outStream, person.photo);
-    emitPhotoData(outStream, person.photo);
+    emitPhotoData(outStream, photo);
+    outStream.write(`<p> location: ${photo.location} </p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -16,7 +17,8 @@ function listRecentPhotos(outStream, photos) {
         .filter(p => p.date > recentDateCutoff())
         .forEach(p => {
             outStream.write("<div>\n");
-            emitPhotoData(outStream, p);
+            emitPhotoData(outStream, photo);
+            outStream.write(`<p> location: ${photo.location} </p>\n`);
             outStream.write("</div>\n");
         });
 }
@@ -24,7 +26,5 @@ function listRecentPhotos(outStream, photos) {
 function emitPhotoData(outStream, photo) {
     outStream.write(`<p> title: ${photo.title} </p>\n`);
     outStream.write(`<p> date: ${photo.date.toDateString()} </p>\n`);
-    outStream.write(`<p> location: ${photo.location} </p>\n`);
 }
-
 
